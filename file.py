55244c63fceb919baa4871b574fd0358c39a1135
Muhammad Tariq <63507947+usamatrq94@users.noreply.github.com>
2022-01-28 15:58:28 +1000

    testing_web_hooks

--- file.py
+++ file.py
@@ -1,1 +1,10 @@
-This is the original file
+import pandas as pd
+import matplotlib.pyplot as plt
+
+data = {
+    "test": [0,1,2,3,4,5],
+    "results": ["a", "b", "c","d", "e"]
+}
+
+df = pd.DataFrame(data=data)
+df.head
